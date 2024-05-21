4e6eef40d6fcd481b64f647d311e78a0fedfe633
eoBattisti <nickgabrielbd@gmail.com>
2024-05-21 16:10:48 -0300

    feat: add jump buffering

--- scripts/state_machine/states/fall.gd
+++ scripts/state_machine/states/fall.gd
@@ -8,6 +8,7 @@ class_name FallState
 @export var wall_friction: float = 0
 @export var wall_jump_time: float = 0.5
 @onready var coyote_timer = $"../CoyoteTimer"
+@onready var jump_buffer = $"../JumpBuffer"
 
 var can_jump = true
 var wall_jump_timer: float
@@ -17,6 +18,14 @@ func enter() -> void:
 	coyote_timer.start()
 	wall_jump_timer = wall_jump_time
 
+func process_input(event: InputEvent) -> State:
+	
+	if !coyote_timer.is_stopped() and get_jump():
+		jump_state.enter()
+	else:
+		jump_buffer.start()
+	return null
+
 func process_physics(delta: float) -> State:
 	parent.velocity.y += gravity * delta
 	
@@ -24,18 +33,20 @@ func process_physics(delta: float) -> State:
 	
 	if movement != 0:
 		animations.flip_h = movement < 0
+
 	parent.velocity.x = movement
 	parent.move_and_slide()
-	
-	if !coyote_timer.is_stopped() and get_jump():
-		jump_state.enter()
-	
+
 	if parent.is_on_wall_only():
 		if get_jump() and can_jump:
 			can_jump = false
 			jump_state.enter()
 		else:
 			parent.velocity.y = min(parent.velocity.y, wall_friction)
+
+	if parent.is_on_floor() and !jump_buffer.is_stopped():
+		jump_buffer.stop()
+		jump_state.enter()
 
 	if parent.is_on_floor():
 		if movement != 0:
