b0387444e81aeba7aceec85949f6cc46e837208a
eoBattisti <nickgabrielbd@gmail.com>
2024-05-31 17:25:59 -0300

    fix: player does not attack while in rest state

--- scripts/state_machine/attack_states/idle.gd
+++ scripts/state_machine/attack_states/idle.gd
@@ -8,7 +8,10 @@ func enter() -> void:
 	
 
 func process_input(event: InputEvent) -> State:
-	if Input.is_action_just_pressed("attack") and parent.is_on_floor():
+	if (Input.is_action_just_pressed("attack")
+		and parent.is_on_floor()
+		and not parent.movement_state_machine.current_state is RestState
+	):
 		return attacking_state
 	return null
 	
